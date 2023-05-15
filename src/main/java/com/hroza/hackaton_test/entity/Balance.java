f1bcb7f218101bc74ad7dd6ef050bf111c7a4085
Pashanko <pnenchyn@gmail.com>
2023-05-15 22:49:51 +0300

    Initial commit

---
+++ src/main/java/com/hroza/hackaton_test/entity/Balance.java
@@ -0,0 +1,4 @@
+package com.hroza.hackaton_test.entity;
+
+public class Balance {
+}
---
+++ src/test/java/com/hroza/hackaton_test/HackatonTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hroza.hackaton_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
