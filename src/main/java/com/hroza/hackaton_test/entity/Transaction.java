e001b8a046cf17371320f68eaa9a30cd7a1811d0
Pashanko <pnenchyn@gmail.com>
2023-05-17 23:29:16 +0300

    Rewriting entities #1

---
+++ src/main/java/com/hroza/hackaton_test/entity/Transaction.java
@@ -0,0 +1,33 @@
+package com.hroza.hackaton_test.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@NoArgsConstructor
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+}
